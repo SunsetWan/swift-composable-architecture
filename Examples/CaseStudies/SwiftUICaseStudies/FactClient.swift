2f20b58b9fe0dd6b024f43bb0e1f345388414c8b
Sunset Wan <sunset.wan@assaabloy.com>
2025-02-07 13:36:25 +0800

    add `fetchAsync`

--- Examples/CaseStudies/SwiftUICaseStudies/FactClient.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/FactClient.swift
@@ -4,35 +4,38 @@ import Foundation
 import XCTestDynamicOverlay
 
 struct FactClient {
-  var fetch: (Int) -> Effect<String, Failure>
-
-  struct Failure: Error, Equatable {}
+    var fetch: (Int) -> Effect<String, Failure>
+    var fetchAsync: (Int) async throws -> String
+    struct Failure: Error, Equatable {}
 }
 
 // This is the "live" fact dependency that reaches into the outside world to fetch trivia.
 // Typically this live implementation of the dependency would live in its own module so that the
 // main feature doesn't need to compile it.
 extension FactClient {
-  static let live = Self(
-    fetch: { number in
-      Effect.task {
-        try await Task.sleep(nanoseconds: NSEC_PER_SEC)
-        let (data, _) = try await URLSession.shared
-          .data(from: URL(string: "http://numbersapi.com/\(number)/trivia")!)
-        return String(decoding: data, as: UTF8.self)
-      }
-      .mapError { _ in Failure() }
-      .eraseToEffect()
-    }
-  )
+    static let live = Self(
+        fetch: { number in
+            Effect.task {
+                try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(number)/trivia")!)
+                return String(decoding: data, as: UTF8.self)
+            }
+            .mapError { _ in Failure() }
+            .eraseToEffect()
+        }, fetchAsync: { _ in
+            fatalError()
+        }
+    )
 }
 
 #if DEBUG
-  extension FactClient {
-    // This is the "unimplemented" fact dependency that is useful to plug into tests that you want
-    // to prove do not need the dependency.
-    static let unimplemented = Self(
-      fetch: { _ in .unimplemented("\(Self.self).fetch") }
-    )
-  }
+    extension FactClient {
+        // This is the "unimplemented" fact dependency that is useful to plug into tests that you want
+        // to prove do not need the dependency.
+        static let unimplemented = Self(
+            fetch: { _ in .unimplemented("\(Self.self).fetch") },
+            fetchAsync: { _ in fatalError() }
+        )
+    }
 #endif
